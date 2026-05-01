b4380bf92fe112ceaffda656cf2acb56b92a934c
Addi3 <addiec787@gmail.com>
2026-05-01 21:04:13 +0100

    fix: reverted cc methods cus i found the actual iris info stuff

--- src/main/java/net/povstalec/sgjourney/common/compatibility/cctweaked/methods/IrisMethods.java
+++ src/main/java/net/povstalec/sgjourney/common/compatibility/cctweaked/methods/IrisMethods.java
@@ -5,14 +5,9 @@ import dan200.computercraft.api.lua.ILuaContext;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.lua.MethodResult;
 import dan200.computercraft.api.peripheral.IComputerAccess;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.povstalec.sgjourney.common.block_entities.stargate.IrisStargateEntity;
 import net.povstalec.sgjourney.common.block_entities.tech_interface.AbstractInterfaceEntity;
 import net.povstalec.sgjourney.common.compatibility.computer_functions.IrisFunctions;
-import net.povstalec.sgjourney.common.init.SoundInit;
 
 public class IrisMethods
 {
@@ -23,7 +18,7 @@ public class IrisMethods
 		{
 			return "getIris";
 		}
-		
+
 		@Override
 		public MethodResult use(IComputerAccess computer, ILuaContext context, AbstractInterfaceEntity interfaceEntity, IrisStargateEntity stargate, IArguments arguments) throws LuaException
 		{
@@ -42,8 +37,6 @@ public class IrisMethods
 		@Override
 		public MethodResult use(IComputerAccess computer, ILuaContext context, AbstractInterfaceEntity interfaceEntity, IrisStargateEntity stargate, IArguments arguments) throws LuaException
 		{
-			playIrisSound(stargate, "close");
-
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.closeIris(interfaceEntity)});
 		}
 	}
@@ -59,32 +52,10 @@ public class IrisMethods
 		@Override
 		public MethodResult use(IComputerAccess computer, ILuaContext context, AbstractInterfaceEntity interfaceEntity, IrisStargateEntity stargate, IArguments arguments) throws LuaException
 		{
-			playIrisSound(stargate, "open");
-
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.openIris(interfaceEntity)});
 		}
 	}
 
-	private static void playIrisSound(IrisStargateEntity stargate, String action)
-	{
-		if(stargate == null || stargate.getLevel() == null || stargate.getLevel().isClientSide)
-			return;
-
-		ServerLevel level = (ServerLevel) stargate.getLevel();
-		BlockPos pos = stargate.getBlockPos();
-
-		if(action.equals("open"))
-		{
-			level.playSound(null, pos, SoundInit.IRIS_OPEN.get(),
-					SoundSource.BLOCKS, 1.0F, 1.0F);
-		}
-		else if(action.equals("close"))
-		{
-			level.playSound(null, pos, SoundInit.IRIS_CLOSE.get(),
-					SoundSource.BLOCKS, 1.0F, 1.0F);
-		}
-	}
-	
 	public static class StopIris implements InterfaceMethod<IrisStargateEntity>
 	{
 		@Override
@@ -99,7 +70,7 @@ public class IrisMethods
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.stopIris(interfaceEntity)});
 		}
 	}
-	
+
 	public static class GetIrisProgress implements InterfaceMethod<IrisStargateEntity>
 	{
 		@Override
@@ -114,7 +85,7 @@ public class IrisMethods
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.getIrisProgress(stargate)});
 		}
 	}
-	
+
 	public static class GetIrisProgressPercentage implements InterfaceMethod<IrisStargateEntity>
 	{
 		@Override
@@ -129,7 +100,7 @@ public class IrisMethods
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.getIrisProgressPercentage(stargate)});
 		}
 	}
-	
+
 	public static class GetIrisDurability implements InterfaceMethod<IrisStargateEntity>
 	{
 		@Override
@@ -137,14 +108,14 @@ public class IrisMethods
 		{
 			return "getIrisDurability";
 		}
-		
+
 		@Override
 		public MethodResult use(IComputerAccess computer, ILuaContext context, AbstractInterfaceEntity interfaceEntity, IrisStargateEntity stargate, IArguments arguments) throws LuaException
 		{
 			return context.executeMainThreadTask(() -> new Object[] {IrisFunctions.getIrisDurability(stargate)});
 		}
 	}
-	
+
 	public static class GetIrisMaxDurability implements InterfaceMethod<IrisStargateEntity>
 	{
 		@Override
@@ -152,7 +123,7 @@ public class IrisMethods
 		{
 			return "getIrisMaxDurability";
 		}
-		
+
 		@Override
 		public MethodResult use(IComputerAccess computer, ILuaContext context, AbstractInterfaceEntity interfaceEntity, IrisStargateEntity stargate, IArguments arguments) throws LuaException
 		{
